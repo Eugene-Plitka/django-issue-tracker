49773cbcb28048bce9122041d71b734ea9c13c29
Eugene-Plitka <eugene.plitka@gmail.com>
2026-09-24 18:42:37 +0300

    Add workspace model

--- workspaces/models.py
+++ workspaces/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Workspace(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
