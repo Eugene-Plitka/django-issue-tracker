9986380d08ed48bec26b3a696b7e79f33030dae5
Eugene-Plitka <eugene.plitka@gmail.com>
2026-09-24 18:57:23 +0300

    Add workspace string representation

--- workspaces/models.py
+++ workspaces/models.py
@@ -7,3 +7,6 @@ class Workspace(models.Model):
     slug = models.SlugField(max_length=150, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
